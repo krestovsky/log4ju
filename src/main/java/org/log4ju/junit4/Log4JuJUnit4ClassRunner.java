9608cb02b3c95689ea6b8c3ee01db05eec57db3d
unknown <krestovskiy.mg@krestovkiy-mg.fcc.land>
2013-09-13 01:15:15 +0400

    Apache 2.0 license and javadocs

--- src/main/java/org/log4ju/junit4/Log4JuJUnit4ClassRunner.java
+++ src/main/java/org/log4ju/junit4/Log4JuJUnit4ClassRunner.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2013 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.log4ju.junit4;
 
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -7,6 +22,25 @@ import org.junit.runners.model.Statement;
 import org.log4ju.annotation.Message;
 import org.log4ju.annotation.Messages;
 
+/**
+ * <p>
+ * <code>Log4JuJUnit4ClassRunner</code> is a extension of
+ * {@link BlockJUnit4ClassRunner}
+ * </p>
+ * <p>
+ * The following list constitutes all annotations currently supported directly
+ * by <code>Log4JuJUnit4ClassRunner</code>.
+ * </p>
+ * <ul>
+ * <li>{@link Messages &#064;Messages(messages=...)}</li>
+ * <li>{@link Message &#064;Message}</li>
+ * </ul>
+ * <p>
+ * <b>NOTE:</b> <code>Log4JuJUnit4ClassRunner</code> requires JUnit 4.5+.
+ * </p>
+ * 
+ * @author Maxim Krestovsky
+ */
 public class Log4JuJUnit4ClassRunner extends BlockJUnit4ClassRunner {
 	
 	public Log4JuJUnit4ClassRunner(Class<?> klass) throws InitializationError {
--- src/main/java/org/log4ju/junit4/MessagesStatement.java
+++ src/main/java/org/log4ju/junit4/MessagesStatement.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2013 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.log4ju.junit4;
 
 import java.lang.reflect.Method;
@@ -9,6 +24,13 @@ import org.log4ju.LogTester;
 import org.log4ju.annotation.Message;
 import org.log4ju.model.LogMessage;
 
+/**
+ * <code>MessagesStatement</code> is a custom JUnit 4.5+
+ * {@link Statement} which adds support for <em>Log4Junit</em> {@link Messages}, {@link Message}
+ * annotations to test logs which produces annotated method.
+ * 
+ * @author Maxim Krestovsky
+ */
 public class MessagesStatement extends Statement {
 
 	private final Statement next;
